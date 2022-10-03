d526337e8c5c8d66d36a3e44a3f5ce3d672b5e6d
PHLORA <58184377+PHLORA@users.noreply.github.com>
2022-10-03 20:18:25 +0300

    Done connection with Db

--- MouseShop/Program.cs
+++ MouseShop/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MouseShop.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 
 
 var app = builder.Build();
